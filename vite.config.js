81fa2f148feb5a1a737b1271b4a08cfb936ab7d8
Erdem Çevik <hello@erdemcevik.com>
2026-06-01 17:43:49 +0300

    feat: Add Spectre Adworks corporate identity case study page

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,8 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        lidio: resolve(__dirname, 'lidio.html')
+        lidio: resolve(__dirname, 'lidio.html'),
+        spectre: resolve(__dirname, 'spectre.html')
       }
     }
   }
