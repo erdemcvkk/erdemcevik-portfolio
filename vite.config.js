108778bb40c413be0919caeb60ecc2d3dd443f8e
Erdem Çevik <hello@erdemcevik.com>
2026-06-01 14:41:37 +0300

    fix: Add lidio.html to Vite build options to fix Vercel 404

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
+import { resolve } from 'path'
 
 export default defineConfig({
   server: {
     port: 3000
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        lidio: resolve(__dirname, 'lidio.html')
+      }
+    }
   }
 })
